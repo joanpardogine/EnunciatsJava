2d036328c4008bcad0b6904e7c3bd9344b4e5a6f
joanpardogine <joanpardo@ginebro.cat>
2021-04-22 11:13:11 +0200

    Update fitxers de Adressa!

--- OrientatsObjectes/Adressa.java
+++ OrientatsObjectes/Adressa.java
@@ -11,54 +11,54 @@
  * @author Joan
  * */
 public class Adressa {
-	// Atributs 
-	private String localitat;
-	private String codiPostal;
-	private String carrer;
-	
-	// Constructor
-	Adressa(String localitatRebuda,
-			String cpRebut,
-			String carrerRebut){
-		this.localitat = localitatRebuda;
-		this.codiPostal = cpRebut;
-		this.carrer = carrerRebut;
-	}
-	
-	// Getters i Setters
-	
-	public String getLocalitat() {
-		return this.localitat;
-	}
+   // Atributs 
+   private String localitat;
+   private String codiPostal;
+   private String carrer;
+   
+   // Constructor
+   Adressa(String localitatRebuda,
+         String cpRebut,
+         String carrerRebut){
+      this.localitat = localitatRebuda;
+      this.codiPostal = cpRebut;
+      this.carrer = carrerRebut;
+   }
+   
+   // Getters i Setters
+   
+   public String getLocalitat() {
+      return this.localitat;
+   }
 
-	public String getCodiPostal() {
-		return this.codiPostal;
-	}
+   public String getCodiPostal() {
+      return this.codiPostal;
+   }
 
-	public String getCarrer() {
-		return this.carrer;
-	}
-	
-	public void setLocalitat(String localitatRebuda) {
-		this.localitat = localitatRebuda;
-	}
-	
-	public void setCodiPostal(String codiPostalRebut) {
-		this.codiPostal = codiPostalRebut;
-	}
-	
-	public void setCarrer(String carrerRebut) {
-		this.carrer = carrerRebut;
-	}
-	
-	// Mètodes
-	/** Retorna en una cadena 
-	 *  l’adreça sencera amb el format
-	 *  "Carrer – Codi Postal - Localitat".
-	 */  
-	public String toString() {
-		return (this.carrer
-				+ " – " + this.codiPostal
-				+ " – " + this.localitat);
-	}
+   public String getCarrer() {
+      return this.carrer;
+   }
+   
+   public void setLocalitat(String localitatRebuda) {
+      this.localitat = localitatRebuda;
+   }
+   
+   public void setCodiPostal(String codiPostalRebut) {
+      this.codiPostal = codiPostalRebut;
+   }
+   
+   public void setCarrer(String carrerRebut) {
+      this.carrer = carrerRebut;
+   }
+   
+   // Mètodes
+   /** Retorna en una cadena 
+    *  l’adreça sencera amb el format
+    *  "Carrer – Codi Postal - Localitat".
+    */  
+   public String toString() {
+      return (this.carrer
+            + " – " + this.codiPostal
+            + " – " + this.localitat);
+   }
 }
--- OrientatsObjectes/ProgramaPersona2.java
+++ OrientatsObjectes/ProgramaPersona2.java
@@ -2,42 +2,87 @@ import java.util.Scanner;
 
 public class ProgramaPersona2 {
 
-	static Scanner teclat = new Scanner (System.in);
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   static Scanner teclat = new Scanner (System.in);
+   static Scanner teclatNombres = new Scanner (System.in);
+   
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
 
-		String nomPersona1, localitatPersona1, cpPersona1, carrerPersona1;
-		int dniPersona1;
-		
-		System.out.print("Entra el nom de la persona: ");
-		nomPersona1 = teclat.next();
-		
-		System.out.print("Entra el dni de " + nomPersona1 + ": ");
-		dniPersona1 = teclat.nextInt();
-		
-		System.out.print("Entra el localitat de " + nomPersona1 + ": ");
-		localitatPersona1 = teclat.next();
-		
-		System.out.print("Entra el codi postal de " + nomPersona1 + ": ");
-		cpPersona1 = teclat.next();
-		
-		System.out.print("Entra el carrer de " + nomPersona1 + ": ");
-		carrerPersona1 = teclat.next();
-		
-		PersonaAmbAdressa persona1 = new PersonaAmbAdressa(dniPersona1, nomPersona1, localitatPersona1, cpPersona1, carrerPersona1);
-		
-		System.out.println("Persona creada correctament!");
-		System.out.println("Nom         : " + persona1.getNom());
-		System.out.println("DNI         : " + persona1.getDNI());
-		System.out.println("Adressa     : " + persona1.getAdressa());
-		System.out.println("localitat   : " + persona1.getLocalitat());
-		System.out.println("codi postal : " + persona1.getCodiPostal());
-		System.out.println("Carrer 1    : " + persona1.getCarrer2());
-		
-		System.out.println("Carrer 2    : " + persona1.adressaPersona.getCarrer());
-	
-		
-	}
+      String nomPersona1, localitatPersona1, cpPersona1, carrerPersona1;
+      int dniPersona1;
+      
+      System.out.println("----  Prova de GETTERS!  ----");
+      System.out.print("Entra el nom de la persona: ");
+      nomPersona1 = teclat.nextLine();
+      
+      System.out.print("Entra el dni de " + nomPersona1 + ": ");
+      dniPersona1 = teclatNombres.nextInt();
+      
+      System.out.print("Entra la localitat de " + nomPersona1 + ": ");
+      localitatPersona1 = teclat.nextLine();
+      
+      System.out.print("Entra el codi postal de " + nomPersona1 + ": ");
+      cpPersona1 = teclat.nextLine();
+      
+      System.out.print("Entra el carrer de " + nomPersona1 + ": ");
+      carrerPersona1 = teclat.nextLine();
+      
+      PersonaAmbAdressa persona1 = new PersonaAmbAdressa(dniPersona1, nomPersona1, localitatPersona1, cpPersona1, carrerPersona1);
+
+      System.out.println("Persona creada correctament!");
+
+      System.out.println("Nom         : " + persona1.getNom());
+      System.out.println("DNI         : " + persona1.getDNI());
+      System.out.println("Adressa     : " + persona1.getAdressa());
+      System.out.println("localitat   : " + persona1.getLocalitat());
+      System.out.println("codi postal : " + persona1.getCodiPostal());
+      System.out.println("Carrer      : " + persona1.getCarrer());
+      System.out.println("-----------------------------");
+      
+      
+      System.out.println("----  Prova de SETTERS!  ----");
+      // teclat.reset();
+      System.out.print("Entra el NOU nom de "
+            + persona1.getNom() 
+            + ": ");
+      nomPersona1 = teclat.nextLine();
+      persona1.setNom(nomPersona1);
+
+      System.out.print("Entra el NOU dni de "
+            + nomPersona1
+            + " (" + persona1.getDNI()
+            + "): ");
+      dniPersona1 = teclatNombres.nextInt();
+      persona1.setDNI(dniPersona1);
+      
+      System.out.print("Entra la NOVA localitat de "+ nomPersona1
+            + " (" + persona1.getLocalitat()
+            + "): ");
+      localitatPersona1 = teclat.nextLine();
+      persona1.setLocalitat(localitatPersona1);
+      
+      System.out.print("Entra el NOU codi postal de "+ nomPersona1
+            + " (" + persona1.getCodiPostal()
+            + "): ");
+      cpPersona1 = teclat.nextLine();
+      persona1.setCodiPostal(cpPersona1);
+      
+      System.out.print("Entra el NOU carrer de " + nomPersona1
+            + " (" + persona1.getCarrer()
+            + "): ");
+      carrerPersona1 = teclat.nextLine();
+      persona1.setCarrer(carrerPersona1);
+      
+      System.out.println("Persona modificada correctament!");
+      System.out.println("Prova de SETTERS!");
+      System.out.println("Nom         : " + persona1.getNom());
+      System.out.println("DNI         : " + persona1.getDNI());
+      System.out.println("Adressa     : " + persona1.getAdressa());
+      System.out.println("localitat   : " + persona1.getLocalitat());
+      System.out.println("codi postal : " + persona1.getCodiPostal());
+      System.out.println("Carrer      : " + persona1.getCarrer());
+      
+      System.out.println("-----------------------------");
+   }
 
 }
